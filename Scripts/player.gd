a7c43b01706b74a4147e70d1043cafef1d9ace60
John <john.freddy.belalcazar@correounivalle.edu.co>
2025-11-23 16:28:34 -0500

    Fix Player vertical movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -14,7 +14,7 @@ func player_movement(delta):
 	
 	var input = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	)
 	
 	velocity = input.normalized() * speed
